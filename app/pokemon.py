7c3a3901bdaf21724b3190d12df764891a27dc33
Shuprovo Sikder <66190617+ssikder20@users.noreply.github.com>
2021-06-02 11:53:06 -0400

    Update pokemon.py

--- app/pokemon.py
+++ app/pokemon.py
@@ -1,46 +1,85 @@
+type_num = {
+    0: 'Normal',
+    1: 'Fighting',
+    2: 'Flying',
+    3: 'Poison',
+    4: 'Ground',
+    5: 'Rock',
+    6: 'Bug',
+    7: 'Ghost',
+    8: 'Steel',
+    9: 'Fire',
+    10: 'Water',
+    11: 'Grass',
+    12: 'Electric',
+    13: 'Psychic',
+    14: 'Ice',
+    15: 'Dragon',
+    16: 'Dark',
+    17: 'Fairy'
+}
+
+type_effectiveness = {
+    'Normal': [1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    'Fighting': [1, 1, 2, 1, 1, 0.5, 0.5, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0.5, 2],
+    'Flying': [1, 0.5, 1, 1, 0, 2, 0.5, 1, 1, 1, 1, 0.5, 2, 1, 2, 1, 1, 1],
+    'Poison': [1, 0.5, 1, 0.5, 2, 1, 0.5, 1, 1, 1, 1, 0.5, 1, 2, 1, 1, 1, 0.5],
+    'Ground': [1, 1, 1, 0.5, 1, 0.5, 1, 1, 1, 1, 2, 2, 0, 1, 2, 1, 1, 1],
+    'Rock': [0.5, 2, 0.5, 0.5, 2, 1, 1, 1, 2, 0.5, 2, 2, 1, 1, 1, 1, 1, 1],
+    'Bug': [1, 0.5, 2, 1, 0.5, 2, 1, 1, 1, 2, 1, 0.5, 1, 1, 1, 1, 1, 1],
+    'Ghost': [0, 0, 1, 0.5, 1, 1, 0.5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+    'Steel': [0.5, 2, 0.5, 0, 2, 0.5, 0.5, 1, 0.5, 2, 1, 0.5, 1, 0.5, 0.5, 0.5, 1, 0.5],
+    'Fire': [1, 1, 1, 1, 2, 2, 0.5, 1, 0.5, 0.5, 2, 0.5, 1, 1, 0.5, 1, 1, 0.5],
+    'Water': [1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0.5, 0.5, 2, 2, 1, 0.5, 1, 1, 1],
+    'Grass': [1, 1, 2, 2, 0.5, 1, 2, 1, 1, 2, 0.5, 0.5, 0.5, 1, 2, 1, 1, 1],
+    'Electric': [1, 1, 0.5, 1, 2, 1, 1, 1, 0.5, 1, 1, 1, 0.5, 1, 1, 1, 1, 1],
+    'Psychic': [1, 0.5, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 0.5, 1, 1, 2, 1],
+    'Ice': [1, 2, 1, 1, 1, 2, 1, 1, 2, 2, 1, 1, 1, 1, 0.5, 1, 1, 1],
+    'Dragon': [1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 1, 2, 2, 1, 2],
+    'Dark': [1, 2, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 1, 1, 0, 1, 1, 0.5, 2],
+    'Fairy': [1, 0.5, 1, 2, 1, 1, 0.5, 1, 2, 1, 1, 1, 1, 1, 1, 0, 0.5, 1]
+}
+
+name_hash = {
+    772: 'Type: Null',
+    112: 'Mr. Mime',
+    439: 'Mime Jr.',
+    29: 'Nidoran♀',
+    32: 'Nidoran♂',
+    784: 'Kommo-o',
+    783: 'Hakamo-o',
+    83: 'Farfetch\'d',
+    386: 'Deoxys',
+    413: 'Wormadam',
+    487: 'Giratina',
+    492: 'Shaymin',
+    550: 'Basculin',
+    555: 'Darmanitan',
+    641: 'Tornadus',
+    642: 'Thundurus',
+    645: 'Landorus',
+    647: 'Keldeo',
+    648: 'Meloetta',
+    669: 'Flabébé',
+    670: 'Floette',
+    671: 'Florges',
+    678: 'Meowstic',
+    681: 'Aegislash',
+    710: 'Pumpkaboo',
+    711: 'Gourgeist',
+    741: 'Oricorio',
+    745: 'Lycanroc',
+    746: 'Wishiwashi',
+    774: 'Minior',
+    778: 'Mimikyu',
+    782: 'Jangmo-o',
+    785: 'Tapu Koko',
+    786: 'Tapu Lele',
+    787: 'Tapu Bulu',
+    788: 'Tapu Fini',
+}
+
 def get_type_effectiveness(arr):
-    type_effectiveness = {
-        'Normal': [1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        'Fighting': [1, 1, 2, 1, 1, 0.5, 0.5, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0.5, 2],
-        'Flying': [1, 0.5, 1, 1, 0, 2, 0.5, 1, 1, 1, 1, 0.5, 2, 1, 2, 1, 1, 1],
-        'Poison': [1, 0.5, 1, 0.5, 2, 1, 0.5, 1, 1, 1, 1, 0.5, 1, 2, 1, 1, 1, 0.5],
-        'Ground': [1, 1, 1, 0.5, 1, 0.5, 1, 1, 1, 1, 2, 2, 0, 1, 2, 1, 1, 1],
-        'Rock': [0.5, 2, 0.5, 0.5, 2, 1, 1, 1, 2, 0.5, 2, 2, 1, 1, 1, 1, 1, 1],
-        'Bug': [1, 0.5, 2, 1, 0.5, 2, 1, 1, 1, 2, 1, 0.5, 1, 1, 1, 1, 1, 1],
-        'Ghost': [0, 0, 1, 0.5, 1, 1, 0.5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1],
-        'Steel': [0.5, 2, 0.5, 0, 2, 0.5, 0.5, 1, 0.5, 2, 1, 0.5, 1, 0.5, 0.5, 0.5, 1, 0.5],
-        'Fire': [1, 1, 1, 1, 2, 2, 0.5, 1, 0.5, 0.5, 2, 0.5, 1, 1, 0.5, 1, 1, 0.5],
-        'Water': [1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0.5, 0.5, 2, 2, 1, 0.5, 1, 1, 1],
-        'Grass': [1, 1, 2, 2, 0.5, 1, 2, 1, 1, 2, 0.5, 0.5, 0.5, 1, 2, 1, 1, 1],
-        'Electric': [1, 1, 0.5, 1, 2, 1, 1, 1, 0.5, 1, 1, 1, 0.5, 1, 1, 1, 1, 1],
-        'Psychic': [1, 0.5, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 0.5, 1, 1, 2, 1],
-        'Ice': [1, 2, 1, 1, 1, 2, 1, 1, 2, 2, 1, 1, 1, 1, 0.5, 1, 1, 1],
-        'Dragon': [1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 1, 2, 2, 1, 2],
-        'Dark': [1, 2, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 1, 1, 0, 1, 1, 0.5, 2],
-        'Fairy': [1, 0.5, 1, 2, 1, 1, 0.5, 1, 2, 1, 1, 1, 1, 1, 1, 0, 0.5, 1]
-    }
-
-    type_num = {
-        0: 'Normal',
-        1: 'Fighting',
-        2: 'Flying',
-        3: 'Poison',
-        4: 'Ground',
-        5: 'Rock',
-        6: 'Bug',
-        7: 'Ghost',
-        8: 'Steel',
-        9: 'Fire',
-        10: 'Water',
-        11: 'Grass',
-        12: 'Electric',
-        13: 'Psychic',
-        14: 'Ice',
-        15: 'Dragon',
-        16: 'Dark',
-        17: 'Fairy'
-    }
-
     effectiveness = []
 
     def find_type_effectiveness(n):
@@ -65,45 +104,6 @@ def get_type_effectiveness(arr):
     return type_effective
 
 def pokemon_image(nat_dex):
-    name_hash = {
-        772: 'Type: Null',
-        112: 'Mr. Mime',
-        439: 'Mime Jr.',
-        29: 'Nidoran♀',
-        32: 'Nidoran♂',
-        784: 'Kommo-o',
-        783: 'Hakamo-o',
-        83: 'Farfetch\'d',
-        386: 'Deoxys',
-        413: 'Wormadam',
-        487: 'Giratina',
-        492: 'Shaymin',
-        550: 'Basculin',
-        555: 'Darmanitan',
-        641: 'Tornadus',
-        642: 'Thundurus',
-        645: 'Landorus',
-        647: 'Keldeo',
-        648: 'Meloetta',
-        669: 'Flabébé',
-        670: 'Floette',
-        671: 'Florges',
-        678: 'Meowstic',
-        681: 'Aegislash',
-        710: 'Pumpkaboo',
-        711: 'Gourgeist',
-        741: 'Oricorio',
-        745: 'Lycanroc',
-        746: 'Wishiwashi',
-        774: 'Minior',
-        778: 'Mimikyu',
-        782: 'Jangmo-o',
-        785: 'Tapu Koko',
-        786: 'Tapu Lele',
-        787: 'Tapu Bulu',
-        788: 'Tapu Fini',
-    }
-
     from pokepy import V2Client
     import requests
     from bs4 import BeautifulSoup
@@ -122,45 +122,6 @@ def pokemon_image(nat_dex):
 
 def pokemon_data(nat_dex):
     from pokepy import V2Client
-
-    name_hash = {
-        772: 'Type: Null',
-        112: 'Mr. Mime',
-        439: 'Mime Jr.',
-        29: 'Nidoran♀',
-        32: 'Nidoran♂',
-        784: 'Kommo-o',
-        783: 'Hakamo-o',
-        83: 'Farfetch\'d',
-        386: 'Deoxys',
-        413: 'Wormadam',
-        487: 'Giratina',
-        492: 'Shaymin',
-        550: 'Basculin',
-        555: 'Darmanitan',
-        641: 'Tornadus',
-        642: 'Thundurus',
-        645: 'Landorus',
-        647: 'Keldeo',
-        648: 'Meloetta',
-        669: 'Flabébé',
-        670: 'Floette',
-        671: 'Florges',
-        678: 'Meowstic',
-        681: 'Aegislash',
-        710: 'Pumpkaboo',
-        711: 'Gourgeist',
-        741: 'Oricorio',
-        745: 'Lycanroc',
-        746: 'Wishiwashi',
-        774: 'Minior',
-        778: 'Mimikyu',
-        782: 'Jangmo-o',
-        785: 'Tapu Koko',
-        786: 'Tapu Lele',
-        787: 'Tapu Bulu',
-        788: 'Tapu Fini',
-    }
     client = V2Client()
     pokemon = client.get_pokemon(nat_dex)
     stats = [i.base_stat for i in pokemon.stats]
